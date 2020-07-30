19132b05723b5655d073a7ac2f8e68957c2f6504
josemagb23 <jmgallardob98@gmail.com>
2020-07-30 13:53:06 +0200

    Entrepreneur and Investor added

---
+++ src/main/java/acme/entities/Application.java
@@ -0,0 +1,58 @@
+
+package acme.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import acme.entities.roles.Investor;
+import acme.framework.datatypes.Money;
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Application extends DomainEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	private String				ticker;
+
+	@DateTimeFormat(pattern = "dd-MM-yyyy HH:mm")
+	@NotNull
+	private LocalDateTime		start;
+
+	@NotBlank
+	@Length(max = 255)
+	private String				statement;
+
+	@NotNull
+	private Money				money;
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Investor			investor;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private InvestmentRound		investmentRound;
+
+}
