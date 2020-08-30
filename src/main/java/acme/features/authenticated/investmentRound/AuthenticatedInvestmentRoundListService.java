7535787b4b4161955c3de54d0a8c04ba27834935
adrmadfer <adrmadfer@alum.us.es>
2020-08-30 13:09:25 +0200

    model update

--- src/main/java/acme/features/authenticated/investmentRound/AuthenticatedInvestmentRoundListService.java
+++ src/main/java/acme/features/authenticated/investmentRound/AuthenticatedInvestmentRoundListService.java
@@ -18,6 +18,7 @@ public class AuthenticatedInvestmentRoundListService implements AbstractListServ
 	@Autowired
 	private AuthenticatedInvestmentRoundRepository repository;
 
+
 	@Override
 	public boolean authorise(final Request<InvestmentRound> request) {
 		assert request != null;
@@ -31,7 +32,7 @@ public class AuthenticatedInvestmentRoundListService implements AbstractListServ
 		assert entity != null;
 		assert model != null;
 
-		request.unbind(entity, model, "title");
+		request.unbind(entity, model, "title", "money");
 
 	}
 
